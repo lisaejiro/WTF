6453a923e0a866b1ac01aa55ff575a6ee9f89abb
lisaejiro <lisa_ejiro@yahoo.com>
2023-01-27 22:36:27 +0100

    new commt

---
+++ javascript/script.js
@@ -0,0 +1,17 @@
+// let age= 22
+// if (age < 21){
+//     console.log("You cannot enter the club!")
+// } else{
+//     console.log("welcome!")
+// }
+
+// let age = 100;
+// if (age < 100){
+//     console.log("Not eligible")  
+// } else if (age === 100){
+//     document.write("here is your birthday card")
+// } else{
+//     console.log("not eligible, you already gotten one")
+// }
+let{name,age}={name:"abc",age:1,id:1} 
+console.log(name,age)
